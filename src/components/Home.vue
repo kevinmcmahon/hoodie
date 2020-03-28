697860413d193b7146174512af11537db437c765
Kevin McMahon <kevinmcmahon@users.noreply.github.com>
2020-03-27 21:24:22 -0500

    setup routes

---
+++ src/components/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <img alt="Vue logo" src="../assets/chicago-star.svg">
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data: () => ({
+
+  }),
+};
+</script>
---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from '../components/Home';
+
+Vue.use(Router);
+
+
+export default new Router({
+    mode: 'history',
+    routes: [{
+        path: '/',
+        name: 'home',
+        component: Home
+    },]
+});
